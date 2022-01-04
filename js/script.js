f97d4b902f975ce273dea34e0dcd946b4aef30ae
armands <armands.onukrans@gmail.com>
2022-01-05 01:39:54 +0200

    change first commit

---
+++ js/script.js
@@ -0,0 +1,188 @@
+"use strict";
+
+console.log(2 + 2 != 5);
+
+
+// let incr = 10,
+//     discr = 10;
+
+// incr++;
+// discr--;
+
+// console.log(incr);
+// console.log(discr);
+
+// console.log(5%2);
+// console.log(2+2 === 4);
+
+
+// const urladress = "toys";
+
+// console.log(`https://ss.lv/${urladress}`);
+
+
+// let nameList;
+
+// function showName () {
+    
+//     nameList = 'Armands';
+
+//     function showSurname () {
+//         console.log('Uzvārds Onukrans');
+//     }
+
+//     showSurname();
+// }
+
+// showName();
+// console.log(nameList);
+
+// var nameSD = `pecka`;
+// console.log(nameSD);
+
+
+// const objectsArmands = {
+//     name: "Armands",
+//     surname: "Onukrans",
+//     age: 36,
+//     male: true,
+// };
+
+// // console.log(objectsArmands.age);
+// console.log(objectsArmands['name']);
+
+// let array = ['suns', 'kakis', 'putns'];
+// console.log(array[2]);
+
+
+// const answer = +prompt('Vai Jums ir 18 gadi?', '18');
+
+// console.log(answer);
+
+
+
+// const result = confirm('Vai Jums ir 18 gadi?');
+
+// console.log(result);
+
+
+
+// const answers = [];
+
+// answers[0] = prompt('ka jums iet?', '');
+// answers[1] = prompt('ka jums iet2?', '');
+// answers[2] = prompt('ka jums iet3?', '');
+
+// console.log(typeof (answers));
+
+
+
+
+// let userHello = 'Sveiks';
+// let userName = 'Armands';
+
+// let userTitle = userHello + userName;
+
+// if (0) {
+//     console.log(userHello);
+// } else if (0) {
+//     console.log(userTitle);
+// } else {
+//     console.log(userName);
+// }
+
+
+// let num = 1;
+
+// while (num <= 7) {
+//     console.log(num);
+//     num++;
+
+// }
+
+// let num2 = 3;
+
+// while (num2) {
+//     console.log(num2);
+//     num2--;
+
+// }
+
+// for (let i = 0; i < 6; i++) {
+//     console.log(i);
+//     if (i == 3) break;
+// }
+
+
+// let nameTop = 10 + 2;
+// nameTop /= 2;
+// console.log(nameTop);
+// nameTop -= 2 * 3;
+// console.log(nameTop);
+
+// // Inkrements
+// let userName = 7;
+// userName++;
+// console.log(userName);
+
+// // Dekriments
+// let userNameD = 7;
+// userNameD--;
+// console.log(userNameD);
+
+
+// let userCount = 3;
+// let newUsers = ++userCount;
+// console.log(newUsers);
+
+
+// console.log('Armandss' < 'Armands');
+
+// pirma prioritāte 
+// ir "ne"
+// pec tam "un"
+// tad "vai"
+
+// Vai
+// console.log(false || true || false);
+
+// // Un
+// console.log('33' && false && true);
+
+// // Ne - preteeji apgriez
+// console.log(!true);
+
+
+// console.log(1 && 0 || 2 && 1);
+
+// console.log('35' +- "22");
+
+// -------------------------
+
+// const COLOREYE = 12;
+// let userName = 'Vasja';
+// let user = userName;
+
+// console.log(typeof user + typeof COLOREYE);
+
+// const time = new Date().getHours();
+// let sunnyDay = true;
+
+// if (time > 2) {
+//     console.log('Ar labu nakti');
+// } else {
+//     console.log('labrīt');
+// }
+
+
+// let trueOrFalse = 'let';
+
+// console.log(typeof trueOrFalse);
+
+
+// let carCount = 20;
+
+// carCount = (carCount);
+
+// console.log( carCount);
+
